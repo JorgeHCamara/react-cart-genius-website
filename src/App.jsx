8dff119e7e20e8dac41354ea876eff52291a8852
JorgeHCamara <jorgehcamara@hotmail.com>
2023-09-19 01:01:58 -0300

    chore: about page implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// Importe as bibliotecas necessárias
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './GlobalStyles.css'
 import Login from './pages/Login/Login';
@@ -7,6 +6,7 @@ import UserPage from './pages/UserPage/UserPage';
 import ChatPage from './pages/ChatPage/ChatPage';
 import Menu from './components/Menu/Menu';
 import { AuthProvider } from './components/AuthContext/AuthContext';
+import About from './pages/About/About';
 
 function App() {
 
@@ -20,6 +20,7 @@ function App() {
           <Route path="/create-account" element={<CreateAccount />} />
           <Route path="/user-page" element={<UserPage />} />
           <Route path="/chat" element={<ChatPage />} />
+          <Route path='/about' element={<About />} />
         </Routes>
       </Router>
     </AuthProvider>
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import './About.css'
+
+const About = () => {
+    return (
+        <div className="container">
+          <div className="gradient">
+            <div className="contentContainer">
+              <h1 className="title">Sobre</h1>
+              <p className="aboutText">
+                O Cart Genius é uma aplicação que utiliza tecnologia de IA/Chatbot para auxiliar clientes de empresas a encontrar produtos de forma mais fácil e eficiente.
+              </p>
+              <p className="aboutText">
+                Ao interagir com o nosso chat, os clientes podem fazer perguntas sobre produtos específicos, tais como características, preço, dentre outras informações relevantes. O sistema, por sua vez, utiliza técnicas de processamento de linguagem natural para entender o que o cliente está buscando e fornecer respostas precisas e personalizadas em tempo real.
+              </p>
+              <p className="aboutText">
+                Para experimentar o nosso chat Cart Genius e ter uma experiência de compras inovadora, <Link to='/create-account' className="linkTextAbout">clique aqui</Link>.
+              </p>
+              <div className="creditsContainer">
+                <h2 className="creditsTitle">Feito por:</h2>
+                <p className="creditsText">Jorge Camara (RM93739)</p>
+                <p className="creditsText">Filipe Santos (RM94377)</p>
+                <p className="creditsText">Vitor Madureira (RM94036)</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+};
+
+export default About;
